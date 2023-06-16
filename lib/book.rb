c6503eae99ee5214bf304f64d3d45d341c8a1e72
Antonio <antonio18casta@aol.com>
2023-06-16 13:29:01 -0400

    commit tree 29c320ac27bc681c635d709fe3150ca374c0c894

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+class Book
+    # attr_reader macro if we are setting the value of the attribute inside the #initialize method.
+    # attr_reader macro, followed by the attribute name :name, created a getter method for us.
+    # attr_writer macro, followed by the attribute name :name, created a setter method for us.
+    # attr_accessor gives access to reader & writer methods from one macro.
 
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre
 
+    def initialize(title)
+        @title = title
+    end
+    
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,19 @@
 # Make your shoe class here!
+class Shoe
+    # attr_reader macro if we are setting the value of the attribute inside the #initialize method.
+    # attr_reader macro, followed by the attribute name :name, created a getter method for us.
+    # attr_writer macro, followed by the attribute name :name, created a setter method for us.
+    # attr_accessor gives access to reader & writer methods from one macro.
+
+    attr_reader :brand
+    attr_accessor :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
